f267e928a3b7d3392f0167f578120c480b54ff2a
egoing <egoing@gmail.com>
2023-07-22 21:50:07 +0900

    리액트의 코드는 js가 아니라 jsx이다. jsx는 Js+태그를 쉽게 코드에 포함시킬 수 있도록 고안된 js 확장 언어다.
    onclick -> onClick
    onchange -> onChange
    
    이벤트의 값은 함수다.
    
    props의 값은 read only
    State의 값은 쓰기 가능
    
    state는 배열이다.
    첫번째 원소는 값이다. 읽을 때 쓴다. 수정하면 안된다.
    두번째 원소는 함수다. 수정할 때 호출한다. 파라미터가 변경되었을 때만 컴포넌트가 다시 실행된다.
    
    props, state가 바뀌면 컴포넌트가 다시 랜더링 된다.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
+import {useState} from 'react';
 function Counter(props){
+  let countState = useState(props.initValue);
+  let count = countState[0];
+  let setCount = countState[1];
+  function up(){
+    setCount(count+1);
+  }
   return <div>
     <h1>{props.title}</h1>
-    <button>+</button> {props.initValue}
+    <button onClick={up}>+</button> {count}
   </div>
 }
 function App() {
   return (
     <div>
       <Counter title="불면증 카운터" initValue={10}></Counter>
-      <Counter title="손님 카운터" initValue={10}></Counter>
     </div>
   );
 }
