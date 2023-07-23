1f7626881549af2027b520ce6a449d09ffef8070
egoing <egoing@gmail.com>
2023-07-23 20:18:52 +0900

    리팩토링

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ function Counter({title, initValue}){
   const [count, setCount] = useState(initValue);
   const [step, setStep] = useState(1);
   function up(){
-    setCount(count+step);
+    setCount(count+1);
   }
+  const stepHandler = (evt) => {
+    setStep(Number(evt.target.value));
+  };
   return <div>
     <h1>{title}</h1>
     <button onClick={up}>+</button> 
-    <input type="number" value={step} onChange={(evt)=>{
-      setStep(Number(evt.target.value));
-    }}/>
+    <input type="number" value={step} onChange={stepHandler}/>
     {count}
   </div>
 }
