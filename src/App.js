e0c652cce95e3076be39f190e123db469dd8d9ee
egoing <egoing@gmail.com>
2023-07-23 21:54:49 +0900

    부작용은 useEffect에 격리한다. - 부작용을 쉽게 파악할 수 있다. - 테스팅 할 때 유리하다. - 부작용의 실행 타이밍을 제어할 수 있다. 두번째 파라미터가 없으면 컴포넌트와 함께 실행된다. 빈배열이면 딱 한번 실행된다. 값이 있으면 그 값이 변경 되었을 때 실행된다.
    함수가 정의 될 때 함수 내에서 사용되는 변수는 함수 안에 봉인된다. 클로저라고 한다.
    
    set 함수의 입력값은 값이거나, 함수이다. 함수의 파마니터는 신선한 상태의 값이다. 리턴값이 새로운 상태가 된다.
    
    useEffec의 리턴값은 정리 할 때 사용한다. unmount, 재실행될 때 자동으로 호출된다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 function Counter({title, initValue}){
   const [count, setCount] = useState(initValue);
   const [step, setStep] = useState(1);
@@ -24,10 +24,29 @@ function Counter({title, initValue}){
     </ol>
   </div>
 }
+function CounterUseEffect(){
+  const [count, setCount] = useState(0);
+  console.log('CounterUseEffect', count);
+  useEffect(()=>{
+    console.log('useEffect');
+    const id = setInterval(()=>{
+      console.log('interval');
+      setCount(oldCount=>oldCount+1);
+    }, 1000)
+    return ()=>{
+      console.log('clean');
+      clearInterval(id);
+    }
+  },[]);
+  return <div>
+    <h1>useEffect Counter</h1> {count}
+  </div>
+}
 function App() {
   return (
     <div>
       <Counter title="불면증 카운터" initValue={10}></Counter>
+      <CounterUseEffect></CounterUseEffect>
     </div>
   );
 }
