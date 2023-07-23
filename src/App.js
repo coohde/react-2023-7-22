6058054c79be180645b45dfee50fcc549ec02022
egoing <egoing@gmail.com>
2023-07-23 19:41:27 +0900

    구조 분해 할당을 이용해서 코드를 좀 간결하고, 분명하게 개선

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import {useState} from 'react';
-function Counter(props){
-  let countState = useState(props.initValue);
-  let count = countState[0];
-  let setCount = countState[1];
+function Counter({title, initValue}){
+  const [count, setCount] = useState(initValue);
   function up(){
     setCount(count+1);
   }
   return <div>
-    <h1>{props.title}</h1>
+    <h1>{title}</h1>
     <button onClick={up}>+</button> {count}
   </div>
 }
